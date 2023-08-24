8b7759b5042f4463c5734830af7ad6ba7c6894d7
Technocracy <erud1t3.devs@gmail.com>
2023-08-23 20:48:26 -0400

    added read me for the combined dataset

--- models/modeling.py
+++ models/modeling.py
@@ -1,4 +1,5 @@
 ##############################################################################################################
 # Description: training and testing (algos, nn structure, loss functions,
 # using validation loss to determine epoch number for training).
+# this module should be interchangeable with other modules (
 ##############################################################################################################
