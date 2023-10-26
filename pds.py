0a6d6d0503c07857c88c5f96054decf098b3bc7f
Technocracy <erud1t3.devs@gmail.com>
2023-10-25 22:59:01 -0400

    ready for experiments

--- pds.py
+++ pds.py
@@ -38,7 +38,7 @@ def main():
     loader = sepl.SEPLoader()
     shuffled_train_x, shuffled_train_y, shuffled_val_x, \
         shuffled_val_y, shuffled_test_x, shuffled_test_y = loader.load_from_dir(
-        './cme_and_electron/data')
+        '/home1/jmoukpe2016/keras-functional-api/cme_and_electron/data')
 
     elevateds, seps = count_above_threshold(shuffled_train_y)
     print(f'Sub-Training set: elevated events: {elevateds}  and sep events: {seps}')
@@ -64,9 +64,9 @@ def main():
     # training
     Options = {
         'batch_size': 282,
-        'epochs': 2,
+        'epochs': 10000,
         'patience': 25,
-        'learning_rate': 0.1,
+        'learning_rate': 0.06,
     }
 
     # print options used
