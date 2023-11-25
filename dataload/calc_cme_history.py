cae9d9feb7cc455147a04c441b8c0a62606da7b8
Technocracy <erud1t3.devs@gmail.com>
2023-11-25 15:27:18 -0500

    raw done

--- dataload/calc_cme_history.py
+++ dataload/calc_cme_history.py
@@ -5,66 +5,88 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def parse_datetime(date_str: str, format_str: str = "%m/%d/%Y %H:%M") -> datetime:
+def parse_datetime(date_str, format_str: str = "%m/%d/%Y %H:%M") -> datetime:
     """
-    Parse a date string into a datetime object.
+    Parse a date string into a datetime object, or return the input if it is already a datetime.
 
     Parameters:
-    - date_str (str): The date string to parse.
+    - date_str (str or datetime): The date string or datetime object to parse.
     - format_str (str): The format of the date string.
 
     Returns:
-    - datetime: The parsed datetime object.
+    - datetime: The parsed datetime object or the original datetime object.
     """
-    return datetime.strptime(date_str, format_str)
+    if isinstance(date_str, datetime):
+        return date_str
+    elif pd.isna(date_str):
+        return None
+    else:
+        return datetime.strptime(date_str, format_str)
 
 
-def cme_counts_in_timeframe(df: pd.DataFrame, current_time: str, hours: int) -> int:
+
+def cme_counts_in_timeframe(df: pd.DataFrame, current_time, hours: int) -> int:
     """
     Count the number of CMEs within a certain number of hours before the given time,
     excluding the CME at the exact current time.
     """
-    current_dt = parse_datetime(current_time)
+    if isinstance(current_time, str):
+        current_dt = parse_datetime(current_time)
+    else:
+        current_dt = current_time
+    if not current_dt:
+        return 0  # Handle None or invalid dates
     time_threshold = current_dt - timedelta(hours=hours)
     return df[df['CME_DONKI_time'].apply(
         lambda x: current_dt > parse_datetime(x) >= time_threshold)].shape[0]
 
 
-def cme_counts_with_speed_threshold(df: pd.DataFrame, current_time: str, hours: int, speed: int) -> int:
+def cme_counts_with_speed_threshold(df: pd.DataFrame, current_time, hours: int, speed: int) -> int:
     """
     Count the number of CMEs with speed over a certain threshold within a certain number of hours
     before the given time, excluding the CME at the exact current time.
     """
-    current_dt = parse_datetime(current_time)
+    if isinstance(current_time, str):
+        current_dt = parse_datetime(current_time)
+    else:
+        current_dt = current_time
+    if not current_dt:
+        return 0  # Handle None or invalid dates
     time_threshold = current_dt - timedelta(hours=hours)
     return df[(df['CME_DONKI_time'].apply(
         lambda x: current_dt > parse_datetime(x) >= time_threshold)) & (
                       df['CME_DONKI_speed'] > speed)].shape[0]
 
 
-def max_cme_speed_in_timeframe(df: pd.DataFrame, current_time: str, hours: int) -> int:
+def max_cme_speed_in_timeframe(df: pd.DataFrame, current_time, hours: int) -> int:
     """
     Get the maximum CME speed within a certain number of hours before the given time,
     excluding the CME at the exact current time.
     """
-    current_dt = parse_datetime(current_time)
+    if isinstance(current_time, str):
+        current_dt = parse_datetime(current_time)
+    else:
+        current_dt = current_time
+    if not current_dt:
+        return 0  # Handle None or invalid dates
     time_threshold = current_dt - timedelta(hours=hours)
     subset_df = df[
         df['CME_DONKI_time'].apply(lambda x: current_dt > parse_datetime(x) >= time_threshold)]
     return subset_df['CME_DONKI_speed'].max() if not subset_df.empty else 0
 
 
-def cme_statistics_for_row(df: pd.DataFrame, current_time: str) -> Tuple[int, int, int, int]:
+def cme_statistics_for_row(df: pd.DataFrame, current_time) -> Tuple[int, int, int, int]:
     """
     Calculate various CME statistics for a specific row based on the current time.
 
     Parameters:
     - df (pd.DataFrame): The DataFrame containing CME information.
-    - current_time (str): The current time to use as a reference.
+    - current_time (str or datetime): The current time to use as a reference.
 
     Returns:
-    - Tuple[int, int, int, int]: A tuple containing the number of CMEs in the past month, the number of CMEs in the past 9 hours,
-                                 the number of CMEs with speed over 1,000 km/s in the past 9 hours, and the maximum CME speed in the past day.
+    - Tuple[int, int, int, int]: A tuple containing the number of CMEs in the past month,
+      the number of CMEs in the past 9 hours, the number of CMEs with speed over 1,000 km/s
+      in the past 9 hours, and the maximum CME speed in the past day.
     """
     past_month_count = cme_counts_in_timeframe(df, current_time, hours=30 * 24)
     past_9hr_count = cme_counts_in_timeframe(df, current_time, hours=9)
