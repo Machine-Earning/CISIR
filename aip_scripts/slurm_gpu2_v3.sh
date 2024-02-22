b4176001fb61b7ef0ce605a9f390b8ee029850ad
jmoukpe2016 <jmoukpe2016@fit.edu@ai-panther.fit.edu>
2024-02-22 12:01:02 -0500

    script update

---
+++ aip_scripts/slurm_gpu2_v3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=TestJob            # Job name
+#SBATCH --nodes=4                     # Number of nodes
+#SBATCH --ntasks=1                    # Number of tasks
+#SBATCH --mem=32GB                    # Memory per node
+#SBATCH --time=infinite              # Time limit
+#SBATCH --partition=gpu2              # Partition, dynamically set
+#SBATCH --gres=gpu:4                # Number of GPUs per node
+#SBATCH --output=testjob.%J.out       # Output file
+#SBATCH --error=testjob.%J.err        # Error file
+
+echo "Starting at date $(date)"
+
+echo "Running on hosts: $SLURM_NODELIST"
+
+echo "Running on $SLURM_NNODES nodes."
+
+echo "Running on $SLURM_NPROCS processors."
+
+echo "Current working directory is $(pwd)"
+
+# Run the Python module specified as the first argument
+srun python -m $1
+
+# how to use this script:
+# sbatch your_slurm_script.sh sources.mlp_pds
