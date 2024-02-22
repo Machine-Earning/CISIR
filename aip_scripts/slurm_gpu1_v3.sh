7c0fc15d7aabbb27e1cbd289890ab98092ba72c1
jmoukpe2016 <jmoukpe2016@fit.edu@ai-panther.fit.edu>
2024-02-22 12:07:30 -0500

    script update

--- aip_scripts/slurm_gpu1_v3.sh
+++ aip_scripts/slurm_gpu1_v3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=TestJob              # Job name
-#SBATCH --nodes=4                       # Number of nodes gpu5, gpu6, gpu7, gpu8 (max 4 for partition gpu2). if using two nodes then gpu[06-07]
+#SBATCH --nodes=4                       # Number of nodes gpu1, gpu2, gpu3, gpu4 (max 4 for partition gpu2). if using two nodes then gpu[01-02]
 #SBATCH --ntasks=4                      # Number of tasks needs to match number of nodes?
 #SBATCH --mem=32GB                      # Memory per node
 #SBATCH --time=infinite                 # Time limit
-#SBATCH --partition=gpu2                # Partition, dynamically set
+#SBATCH --partition=gpu1                # Partition, dynamically set
 #SBATCH --gres=gpu:4                    # Number of GPUs per node (max 4)
 #SBATCH --output=testjob.%J.out         # Output file
 #SBATCH --error=testjob.%J.err          # Error file
