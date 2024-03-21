ea5a021f0e05b228d402144e3f88b7f96c324936
jmoukpe2016 <jmoukpe2016@fit.edu@ai-panther.fit.edu>
2024-03-21 09:47:19 -0400

    from panther

--- aip_scripts/slurm_gpu2_v2.sh
+++ aip_scripts/slurm_gpu2_v2.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=repl.pds           # Job name
 #SBATCH --nodes=1                     # Number of nodes
 #SBATCH --ntasks=1                    # Number of tasks
-#SBATCH --mem=32GB                    # Memory per node
+#SBATCH --mem=64GB                    # Memory per node
 #SBATCH --time=infinite              # Time limit
 #SBATCH --partition=gpu2              # Partition, dynamically set
 #SBATCH --gres=gpu:4               # Number of GPUs per node
