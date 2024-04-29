3a81dac75038435084657cf37692e0b79f42ec0a
Technocracy <erud1t3.devs@gmail.com>
2024-04-29 13:57:40 -0400

    Update utils.py

--- modules/evaluate/utils.py
+++ modules/evaluate/utils.py
@@ -6,7 +6,8 @@ import numpy as np
 import tensorflow as tf
 from scipy.spatial.distance import pdist
 from scipy.stats import pearsonr
-from sklearn.manifold import TSNE
+# from sklearn.manifold import TSNE
+from tsnecuda import TSNE
 from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import gaussian_kde
 from modules.evaluate import evaluation as eval
