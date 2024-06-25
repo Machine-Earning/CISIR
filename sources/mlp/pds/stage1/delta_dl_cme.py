8429b1bf360b6c4ca78e470f40a41ab35da2da22
Technocracy <erud1t3.devs@gmail.com>
2024-06-25 14:38:46 -0400

    Update delta_dl_cme.py

--- sources/mlp/pds/stage1/delta_dl_cme.py
+++ sources/mlp/pds/stage1/delta_dl_cme.py
@@ -48,7 +48,7 @@ def main():
         for inputs_to_use in [['e0.5', 'e1.8', 'p']]:
             for cme_speed_threshold in [0]:
                 for add_slope in [False]:
-                    for alpha in [0, 0.3, 0.6, 1]:
+                    for alpha in [0, 0.6]:
                         # PARAMS
                         # inputs_to_use = ['e0.5']
                         # add_slope = True
