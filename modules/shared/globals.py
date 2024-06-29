a64beb7676ff6676fae13c2149467ad770fdb8f1
Josias Moukpe <erud1t3.devs@gmail.com>
2024-06-29 11:46:29 -0400

    Update globals.py

--- modules/shared/globals.py
+++ modules/shared/globals.py
@@ -1,35 +1,49 @@
 # Global configurations
-SEED = [456789]
-INPUTS_TO_USE = [['e0.5', 'e1.8', 'p']]
-OUTPUTS_TO_USE = ['delta_p']
-CME_SPEED_THRESHOLD = [0]
-ALPHAS = [0.1, 0.5]
-ADD_SLOPE = [False]
-BATCH_SIZE = 4096
-EPOCHS = int(1e6)
-HIDDENS = [
-    2048, 1024, 2048, 1024, 1024, 512, 1024, 512, 
-    512, 256, 512, 256, 256, 128, 256, 128, 
+SEEDS = [456789]  # Seeds for reproducibility
+INPUTS_TO_USE = [['e0.5', 'e1.8', 'p']]  # Inputs to use
+OUTPUTS_TO_USE = ['delta_p']  # Output to use
+CME_SPEED_THRESHOLD = [0]  # CME speed threshold
+ALPHAS = [0.1, 0.5]  # Alpha values for the loss
+ADD_SLOPE = [False]  # Add slope to the inputs
+BATCH_SIZE = 4096  # Batch size
+EPOCHS = int(1e6)  # Number of epochs
+MLP_HIDDENS = [
+    2048, 1024, 2048, 1024, 1024, 512, 1024, 512,
+    512, 256, 512, 256, 256, 128, 256, 128,
     256, 128, 128, 128, 128, 128, 128, 128
-]
-PROJ_HIDDENS = [64]
-LOSS_KEY = 'mse'
-REPR_DIM = 128
-OUTPUT_DIM = len(OUTPUTS_TO_USE)
-DROPOUT = 0.1
-ACTIVATION = None
-NORM = 'batch_norm'
-RESIDUAL = True
-SKIPPED_LAYERS = 2
+]  # Hidden layers
+PROJ_HIDDENS = [64]  # Projection hidden layers
+LOSS_KEY = 'mse'  # Mean squared error regression loss
+REPR_DIM = 128  # Representation dimension
+OUTPUT_DIM = len(OUTPUTS_TO_USE)  # Number of outputs
+DROPOUT = 0.1  # Dropout rate
+ACTIVATION = None  # No activation for regression
+NORM = 'batch_norm'  # Use batch normalization
+RESIDUAL = True  # Use residual connections
+SKIPPED_LAYERS = 2  # Number of layers to skip in residual connections
 N = 500  # Number of samples to keep outside the threshold
 LOWER_THRESHOLD = -0.5  # Lower threshold for delta_p
 UPPER_THRESHOLD = 0.5  # Upper threshold for delta_p
 MAE_PLUS_THRESHOLD = 0.5  # Threshold for measuring raising edges in delta
-LEARNING_RATE = 3e-3  # Lower due to finetuning
+START_LR_FT = 3e-3  # Lower due to fine-tuning
+START_LR = 1e-2  # starting learning rate
 WEIGHT_DECAY = 1e-6  # Higher weight decay
+WEIGHT_DECAY_PDS = 1e-2  # Higher weight decay for projection layers
 MOMENTUM_BETA1 = 0.9  # Higher momentum beta1
 BANDWIDTH = 4.42e-2  # Bandwidth for rebalancing
 PATIENCE = int(25e3)  # Higher patience
-LR_CB_FACTOR = 0.9
-LR_CB_PATIENCE = 1000
-LR_CB_MIN_LR = 1e-4
+LR_CB_FACTOR = 0.9  # factor for reducing learning rate
+LR_CB_PATIENCE = 1000  # patience for reducing learning rate
+LR_CB_MIN_LR = 1e-4  # minimum learning rate
+VERBOSE = 1  # Verbose
+SAVE_BEST = False  # Save best model
+LR_CB_MIN_DELTA = 1e-5  # Minimum delta for reducing learning rate
+LR_CB_MONITOR = 'loss'  # Monitor validation loss
+DS_VERSION = 6  # Dataset version
+DS_PATH = 'data/electron_cme_data_split_v5'  # Path to the dataset
+VAL_SPLIT = 0.25  # Validation split
+TARGET_MIN_NORM_WEIGHT = 0.01  # Minimum weight for the target normalization
+COMMON_VAL_ALPHA = 1  # Common validation alpha
+ES_CB_MONITOR = 'val_loss'  # Monitor validation loss
+ES_CB_RESTORE_WEIGHTS = False  # Restore weights
+WANDB_SAVE_MODEL = False  # Save model to wandb
