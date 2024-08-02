a2256a49407b372f7fc50e3819fa92526481a0bc
ERUD1T3 <erud1t3.devs@gmail.com>
2024-08-02 09:54:45 -0400

    more tests on sam

--- sources/mlp/delta_cme_sam.py
+++ sources/mlp/delta_cme_sam.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from modules.evaluate.utils import plot_repr_corr_dist, plot_tsne_delta
 
 # Set the environment variable for CUDA (in case it is necessary)
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 import tensorflow as tf
 import wandb
@@ -37,7 +37,7 @@ def main():
         for inputs_to_use in INPUTS_TO_USE:
             for cme_speed_threshold in CME_SPEED_THRESHOLD:
                 for alpha in [0.5]:
-                    for rho in [0.7, 0.9, 1.1, 1.3]: #SAM_RHOS:
+                    for rho in [0.4, 0.6]: #SAM_RHOS:
                         for add_slope in ADD_SLOPE:
                             # PARAMS
                             outputs_to_use = OUTPUTS_TO_USE
