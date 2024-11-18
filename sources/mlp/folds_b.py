587ff1e7a40b7625698e495c2a88dff3983023fa
Josias Moukpe <erud1t3.devs@gmail.com>
2024-11-18 17:15:56 -0500

    about to test

--- sources/mlp/folds_b.py
+++ sources/mlp/folds_b.py
@@ -75,7 +75,7 @@ def main():
                             momentum_beta1 = MOMENTUM_BETA1  # higher momentum beta1
                             batch_size = BATCH_SIZE  # higher batch size
                             epochs = EPOCHS  # higher epochs
-                            hiddens = MLP_HIDDENS_S  # hidden layers
+                            hiddens = MLP_HIDDENS  # hidden layers
 
                             hiddens_str = (", ".join(map(str, hiddens))).replace(', ', '_')
                             bandwidth = BANDWIDTH
