e23ccad21100fa28d156b2231c94e234e425dcd9
Josias Moukpe <erud1t3.devs@gmail.com>
2025-04-08 14:52:23 -0400

    Refactor SEP CME preprocessing notebook for improved analysis and execution tracking
    - Enhanced the `analyze_label_dist_with_kde` function to include additional parameters for better customization.
    - Updated execution counts across code cells to ensure accuracy with the latest changes.
    - Fixed minor issues to streamline data analysis and visualization processes.

---
+++ modules/shared/onp_globals.py
@@ -0,0 +1,64 @@
+# Dataset configurations
+DS_VERSION = 1  # Dataset version
+DS_PATH = 'data/allstate_claims'  # Path to the dataset
+
+OUTPUT_DIM = 1
+
+
+# Training configurations
+TRIAL_SEEDS = [456789, 42, 123, 0, 9999] # Seeds for trial
+BATCH_SIZE = 4096  # Batch size
+EPOCHS = int(2e5)  # Number of epochs
+VERBOSE = 1  # Verbose
+SAVE_BEST = False  # Save best model
+WANDB_SAVE_MODEL = False  # Save model to wandb
+FREEZING = [False]
+
+# Model Architecture
+MLP_HIDDENS = [1024, 64, 512, 64, 256, 64, 128, 64]  # Hidden layers
+
+PROJ_HIDDENS = [32]  # Projection hidden layers
+EMBED_DIM = 64  # Representation dimension
+DROPOUT = 0.5  # Dropout rate
+ACTIVATION = None  # No activation for regression so default is LeakyReLU
+NORM = 'batch_norm'  # Use batch normalization
+RESIDUAL = True  # Use residual connections
+SKIPPED_LAYERS = 1
+SKIP_REPR = True  # residual representation
+
+# Loss and Optimization
+LOSS_KEY = 'cmse'  # Correlated Mean squared error regression loss
+START_LR = 5e-4  # starting learning rate
+WEIGHT_DECAY = 1  # Higher weight decay
+NORMALIZED_WEIGHTS = True  # Use normalized weights
+
+# Learning Rate Scheduling
+LR_CB_MIN_LR = 1e-5  # minimum learning rate
+LR_CB_FACTOR = 0.95  # factor for reducing learning rate # gradual decay leads to more stable training
+LR_CB_PATIENCE = 50  # patience for reducing learning rate
+LR_CB_MIN_DELTA = 1e-5 # Minimum delta for reducing learning rate
+LR_CB_MONITOR = 'loss'  # Monitor validation loss
+
+# Early Stopping
+PATIENCE = int(3e3)  # Higher patience
+ES_CB_MONITOR = 'val_loss'  # Monitor validation loss
+ES_CB_RESTORE_WEIGHTS = True  # Restore weights
+
+# Data Filtering and Processing
+LOWER_THRESHOLD = 4  # Lower threshold for rebalancing
+UPPER_THRESHOLD = 12  # Upper threshold for rebalancing
+BANDWIDTH = 1.331  # Bandwidth for rebalancing
+
+# Smoothing Parameters
+SMOOTHING_METHOD = 'moving_average'
+WINDOW_SIZE = 61  # NOTE: must be odd
+VAL_WINDOW_SIZE = 61 # NOTE: must be odd
+
+# Additional Parameters
+RHO = [0]
+REWEIGHTS = [(0.85, 0.85, 0.0, 0.0)]
+LAMBDA_FACTOR = 1
+CVRG_MIN_DELTA = 1e-3
+CVRG_METRIC = 'val_loss'
+ASYM_TYPE = None #'sigmoid'
+N_FILTER = 500
